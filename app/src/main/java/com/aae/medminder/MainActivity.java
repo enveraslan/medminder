ef02010aa4fa68f940553783a44e5c7f3c738efb
enveraslan <enveraslan@marun.edu.tr>
2020-12-17 09:13:25 +0300

    feat(elements): add horizontal calendar

--- app/src/main/java/com/aae/medminder/MainActivity.java
+++ app/src/main/java/com/aae/medminder/MainActivity.java
@@ -4,19 +4,40 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.aae.medminder.components.calendar.CustomHorizontalCalendar;
+import com.aae.medminder.components.calendar.OnHorizontalDateSelectListener;
+import com.aae.medminder.components.calendar.model.DateModel;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements OnHorizontalDateSelectListener {
     // Initialize variable
     DrawerLayout drawerLayout;
+    private CustomHorizontalCalendar mCalendar;
+
 
     @ Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         drawerLayout = findViewById(R.id.drawer_layout);
+
+        mCalendar = findViewById(R.id.calendar);
+        mCalendar.setOnDateSelectListener(this);
+        mCalendar.setLocale(Locale.getDefault());
+        Calendar calendar = Calendar.getInstance(); // this would default to now
+        calendar.add(Calendar.DAY_OF_MONTH, -10);
+        mCalendar.setStartDate(calendar.getTime());
+
+        calendar.add(Calendar.DAY_OF_MONTH, 45);
+
+        mCalendar.selectDate(Calendar.getInstance().getTime());
     }
 
     public void ClickMenu(View view) {
@@ -74,5 +95,13 @@ public class MainActivity extends AppCompatActivity {
     public void ClickSettings(View view) {
         Toast toast = Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_LONG);
         toast.show();
+    }
+
+    @Override
+    public void onDateClick(DateModel dateModel) {
+
+
+
+        Log.d("date", dateModel != null ? dateModel.month + dateModel.day + dateModel.dayOfWeek + dateModel.year : "");
     }
 }
