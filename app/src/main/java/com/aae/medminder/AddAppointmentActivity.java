cb7d7bf294e2f51a9740648b2da5be8b9f516b04
enveraslan <enveraslan@marun.edu.tr>
2021-01-02 00:25:37 +0300

    feat: create crud process for Appointment

--- app/src/main/java/com/aae/medminder/AddAppointmentActivity.java
+++ app/src/main/java/com/aae/medminder/AddAppointmentActivity.java
@@ -3,20 +3,36 @@ package com.aae.medminder;
 import android.app.DatePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class AddAppointmentActivity extends AppCompatActivity {
+import com.aae.medminder.models.Appointment;
+import com.aae.medminder.models.Doctor;
 
+public class AddAppointmentActivity extends AppCompatActivity {
+    private SimpleDateFormat sdf = new SimpleDateFormat("dd/M/yyyy");
+    private EditText editTextAppointmentTitle;
+    private Spinner spinnerDoctor;
     private EditText dateInput;
     private Button setDateButton;
+    private EditText editTextLocation;
+    private EditText editTextNote;
+    private CheckBox checkBoxIsReminder;
+    private Button buttonSaveAppointment;
     private int mYear, mMonth, mDay;
     Context context = this;
 
@@ -32,8 +48,22 @@ public class AddAppointmentActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
 
+        editTextAppointmentTitle = findViewById(R.id.editTextAppointmentTitle);
+        spinnerDoctor = findViewById(R.id.spinnerDoctor);
+        editTextLocation = findViewById(R.id.editTextLocation);
+        editTextNote = findViewById(R.id.editTextNote);
+        checkBoxIsReminder = findViewById(R.id.checkBoxIsReminder);
+        buttonSaveAppointment = findViewById(R.id.buttonSaveAppointment);
+
+
         setDateButton = findViewById(R.id.setDateButton);
-        dateInput = findViewById(R.id.date_input);
+        dateInput = findViewById(R.id.editTextAppointmentDate);
+
+        List<Doctor> doctorList = MedminderApp.getDaoSession().getDoctorDao().loadAll();
+        doctorList.add(0, new Doctor(null, "Please, select doctor ", "if any.", null, null, null));
+        ArrayAdapter<Doctor> adapterDoctor = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, doctorList);
+        adapterDoctor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDoctor.setAdapter(adapterDoctor);
 
         setDateButton.setOnClickListener(new View.OnClickListener()
         {
@@ -53,12 +83,20 @@ public class AddAppointmentActivity extends AppCompatActivity {
                             @Override
                             public void onDateSet(DatePicker view, int year,
                                                   int monthOfYear, int dayOfMonth) {
-
-                                dateInput.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                                Calendar c = Calendar.getInstance();
+                                c.set(year, monthOfYear,dayOfMonth);
+                                dateInput.setText(sdf.format(c.getTime()));
 
                             }
                         }, mYear, mMonth, mDay);
                 datePickerDialog.show();
+            }
+        });
+
+        buttonSaveAppointment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                save();
             }
         });
 
@@ -70,5 +108,21 @@ public class AddAppointmentActivity extends AppCompatActivity {
         });
     }
 
+    private void save() {
+        Appointment appointment = new Appointment();
+        appointment.setAppointmentID(null);
+        appointment.setTitle(editTextAppointmentTitle.getText().toString());
+        appointment.setDoctor((Doctor)spinnerDoctor.getSelectedItem());
+        appointment.setDate(dateInput.getText().toString());
+        appointment.setLocation(editTextLocation.getText().toString());
+        appointment.setNotes(editTextNote.getText().toString());
+        if (checkBoxIsReminder.isChecked()) {
+            appointment.setReminder(1L);
+        } else {
+            appointment.setReminder(0L);
+        }
+
+        MedminderApp.getDaoSession().getAppointmentDao().insert(appointment);
+    }
 
 }
--- app/src/main/java/com/aae/medminder/models/Appointment.java
+++ app/src/main/java/com/aae/medminder/models/Appointment.java
@@ -3,16 +3,54 @@ package com.aae.medminder.models;
 import org.greenrobot.greendao.annotation.Entity;
 import org.greenrobot.greendao.annotation.Id;
 import org.greenrobot.greendao.annotation.Generated;
+import org.greenrobot.greendao.annotation.Property;
+import org.greenrobot.greendao.annotation.ToOne;
+import org.greenrobot.greendao.DaoException;
 
 @Entity(nameInDb = "Appointment")
 public class Appointment{
 
     @Id (autoincrement = true)
-    private Long AppointmentID;
+    private Long appointmentID;
 
-    @Generated(hash = 1092669253)
-    public Appointment(Long AppointmentID) {
-        this.AppointmentID = AppointmentID;
+    @Property(nameInDb = "title")
+    private String title;
+
+    private Long doctorID;
+
+    @ToOne(joinProperty = "doctorID")
+    private  Doctor doctor;
+
+    @Property (nameInDb = "date")
+    private String date;
+
+    @Property (nameInDb = "location")
+    private String location;
+
+    @Property (nameInDb = "Notes")
+    private String notes;
+
+    @Property (nameInDb = "reminder")
+    private Long reminder;
+
+    /** Used to resolve relations */
+    @Generated(hash = 2040040024)
+    private transient DaoSession daoSession;
+
+    /** Used for active entity operations. */
+    @Generated(hash = 1077317242)
+    private transient AppointmentDao myDao;
+
+    @Generated(hash = 709457673)
+    public Appointment(Long appointmentID, String title, Long doctorID, String date,
+            String location, String notes, Long reminder) {
+        this.appointmentID = appointmentID;
+        this.title = title;
+        this.doctorID = doctorID;
+        this.date = date;
+        this.location = location;
+        this.notes = notes;
+        this.reminder = reminder;
     }
 
     @Generated(hash = 1660940633)
@@ -20,13 +58,135 @@ public class Appointment{
     }
 
     public Long getAppointmentID() {
-        return this.AppointmentID;
+        return this.appointmentID;
     }
 
-    public void setAppointmentID(Long AppointmentID) {
-        this.AppointmentID = AppointmentID;
+    public void setAppointmentID(Long appointmentID) {
+        this.appointmentID = appointmentID;
     }
 
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getDoctorID() {
+        return this.doctorID;
+    }
+
+    public void setDoctorID(Long doctorID) {
+        this.doctorID = doctorID;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getNotes() {
+        return this.notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Long getReminder() {
+        return this.reminder;
+    }
+
+    public void setReminder(Long reminder) {
+        this.reminder = reminder;
+    }
+
+    @Generated(hash = 1650391412)
+    private transient Long doctor__resolvedKey;
+
+    /** To-one relationship, resolved on first access. */
+    @Generated(hash = 1211852376)
+    public Doctor getDoctor() {
+        Long __key = this.doctorID;
+        if (doctor__resolvedKey == null || !doctor__resolvedKey.equals(__key)) {
+            final DaoSession daoSession = this.daoSession;
+            if (daoSession == null) {
+                throw new DaoException("Entity is detached from DAO context");
+            }
+            DoctorDao targetDao = daoSession.getDoctorDao();
+            Doctor doctorNew = targetDao.load(__key);
+            synchronized (this) {
+                doctor = doctorNew;
+                doctor__resolvedKey = __key;
+            }
+        }
+        return doctor;
+    }
+
+    /** called by internal mechanisms, do not call yourself. */
+    @Generated(hash = 1853493926)
+    public void setDoctor(Doctor doctor) {
+        synchronized (this) {
+            this.doctor = doctor;
+            doctorID = doctor == null ? null : doctor.getDoctorID();
+            doctor__resolvedKey = doctorID;
+        }
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#delete(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 128553479)
+    public void delete() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.delete(this);
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#refresh(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 1942392019)
+    public void refresh() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.refresh(this);
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#update(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 713229351)
+    public void update() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.update(this);
+    }
+
+    /** called by internal mechanisms, do not call yourself. */
+    @Generated(hash = 850012473)
+    public void __setDaoSession(DaoSession daoSession) {
+        this.daoSession = daoSession;
+        myDao = daoSession != null ? daoSession.getAppointmentDao() : null;
+    }
 
 
 }
